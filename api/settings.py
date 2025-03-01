71c178fa29a92a9d30a24094e43c38607d2c085b
Allen Sun <allen.haha@hotmail.com>
2025-03-01 16:11:19 +0800

    Fix CORS being too strict

--- api/settings.py
+++ api/settings.py
@@ -1,3 +1,8 @@
+import socket
+
+IP = socket.gethostbyname(socket.gethostname())
+
 DB = "sqlite:///db.sqlite3"
 HOSTS = ["http://localhost:8000", "http://localhost:3000"]
+HOSTS += ["http://"+IP+":8000", "http://"+IP+":3000"]
 RUN_TESTS = False
