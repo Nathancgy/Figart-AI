8aff218df137b879a57c31f551939195613678d8
Allen Sun <allen.haha@hotmail.com>
2025-03-01 09:33:57 +0800

    start debugging

--- api/backend.py
+++ api/backend.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, JSON
+from sqlalchemy import create_engine, Column, Integer, String, JSON, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi import HTTPException
