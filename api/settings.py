7ba96433286771ca56ce28495c08c0217681055c
Nathancgy <guangyu.chen40730-biph@basischina.com>
2025-03-01 16:22:11 +0800

    updated api

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,6 @@
 import socket
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = "192.168.8.116"#socket.gethostbyname(socket.gethostname())
 
 DB = "sqlite:///db.sqlite3"
 HOSTS = ["http://localhost:8000", "http://localhost:3000"]
